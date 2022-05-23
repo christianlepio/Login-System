7530f1c8b644e63749189ae25df338fb7669d1cc
babaGie <rylepio79@gmail.com>
2022-05-23 19:53:21 +0800

    add resources folder

---
+++ resources/views/esystems.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.layout')
+@section('content')
+<section class="hero is-small is-info">
+    <div class="hero-body">
+        <div class="container">
+            <div class="columns is-justify-content-center p-2">
+                <div class="column is-two-fifths mb-5 mt-5">
+                    <form class="box px-5 mb-5 mt-5">
+                        <center>
+                        <figure class="image is-96x96">
+                            <img src="/img/logoko.JPG">
+                        </figure>
+                        </center>
+                        <h1 class="has-text-centered is-size-2 mt-3 mb-5">E - SYSTEMS</h1>
+                        <div class="field">
+                            <label class="label">Email</label>
+                            <div class="control">
+                            <input class="input is-rounded" type="email" placeholder="e.g. alex@example.com">
+                            </div>
+                        </div>
+
+                        <div class="field">
+                            <label class="label">Password</label>
+                            <div class="control">
+                            <input class="input is-rounded mb-3" type="password" placeholder="********">
+                            </div>
+                        </div>
+                        <center>
+                            <button class="button is-primary is-rounded mb-5">Sign In</button>
+                        </center>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+            img{
+                -webkit-clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+                clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+            }button{
+                width:50vmin;
+            }
+        </style>
+    </head>
+    <body>
+    @yield('content')
+    <footer class="footer">
+            <div class="content has-text-centered">
+            <p id="copyright">Copyright &copy; 2022 | E - Systems</p>
+            </div>
+        </footer>
+    </body>
+</html>
