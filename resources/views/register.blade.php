9f62596c866db5a039653eabe8c185261654ac9e
babaGie <rylepio79@gmail.com>
2022-05-26 22:20:54 +0800

    update register bootstrap

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,8 +1,7 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
 @extends('layouts.layout')
 @section('content')
-    <div class="container">
-        hello
-    </div>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <div class="container">
+            hello
+        </div>
 @endsection
