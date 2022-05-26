a5d4d9fb780f6c142b7e62d9ccd2f1dadcb2e1ff
babaGie <rylepio79@gmail.com>
2022-05-26 21:36:02 +0800

    remove  colon

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -14,7 +14,7 @@
                         <div class="col-md-4">
                             <div class="form-floating mb-3">
                                 <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
-                                <label for="floatingInput">First name: </label>
+                                <label for="floatingInput">First name </label>
                             </div>
                         </div>
                     </div>
