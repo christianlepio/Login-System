ff735bac0c475f847d3d1b85bd92abe715b50ea5
babaGie <rylepio79@gmail.com>
2022-05-26 23:00:35 +0800

    add bg-light and padding

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,8 +2,10 @@
 @section('content')
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <div class="container">
-            <div class="row justify-content-center">
-                <form action="" method="post"></form>
+            <div class="bg-light p-4 rounded">
+                <form action="" method="post">
+                    
+                </form>
             </div>
         </div>
 @endsection
