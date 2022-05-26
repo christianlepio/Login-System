95f8b7e5d77d119686a093772b4ccdbe5a4da764
babaGie <rylepio79@gmail.com>
2022-05-26 22:03:33 +0800

    remove bg color

--- resources/views/esystems.blade.php
+++ resources/views/esystems.blade.php
@@ -3,7 +3,7 @@
 
 <!-- This is  for body section or content-->
 @section('content')
-<section class="hero is-small is-info">
+<section class="hero is-small">
     <div class="hero-body">
         <div class="container">
             <div class="columns is-justify-content-center p-2">
