17961aeda7abbc686b7d44f9cfc68aa4428fc090
babaGie <rylepio79@gmail.com>
2022-05-26 22:21:56 +0800

    update routes

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
 
-Route::get('/esystems', function () {
+Route::get('/', function () {
     return view('esystems');
 });
