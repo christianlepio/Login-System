aeedc8fed9a9cd97b8c4a519143dce66eff25767
babaGie <rylepio79@gmail.com>
2022-05-26 21:33:58 +0800

    align center  the 2nd row

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -18,7 +18,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="row"></div>
+                    <div class="row justify-content-center"></div>
                 </form>
             </div>
         </div>
