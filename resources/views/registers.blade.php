6bd5a509188e47ab2131eaa920d2984ca921b436
babaGie <rylepio79@gmail.com>
2022-05-27 08:23:21 +0800

    add cards layout

--- resources/views/registers.blade.php
+++ resources/views/registers.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <div class="container">
-            <div class="p-4 rounded">
+            <div class="card">
                 <form action="" method="post">
                     <div class="row justify-content-center">
                         <div class="col-md-4">
