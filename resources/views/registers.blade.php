15c1e766b838baddbaa5a6bb465042bdfe30e9d6
babaGie <rylepio79@gmail.com>
2022-05-27 08:19:17 +0800

    add password label

--- resources/views/registers.blade.php
+++ resources/views/registers.blade.php
@@ -36,7 +36,7 @@
                         <div class="col-md-4">
                             <div class="form-floating mb-3">
                                 <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                                <label for="floatingInput">Email Address </label>
+                                <label for="floatingInput">Password </label>
                             </div>
                         </div>
                         <div class="col-md-4">
