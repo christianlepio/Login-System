28fc36d1091a5357ac6bc95a2b74d0ef1330c0e4
babaGie <rylepio79@gmail.com>
2022-05-27 08:24:27 +0800

    add card header

--- resources/views/registers.blade.php
+++ resources/views/registers.blade.php
@@ -3,6 +3,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <div class="container">
             <div class="card">
+                <div class="card-header">Registration Module</div>
                 <form action="" method="post">
                     <div class="row justify-content-center">
                         <div class="col-md-4">
