1183c2d5ee61555b5e8c2f40ad56f7f64959e4a5
babaGie <rylepio79@gmail.com>
2022-05-28 21:59:51 +0800

    add card body

--- resources/views/registers.blade.php
+++ resources/views/registers.blade.php
@@ -4,50 +4,52 @@
         <div class="container">
             <div class="card mt-5 mx-1">
                 <div class="card-header fs-4 fw-bold">Registration Module</div>
-                <form action="" method="post">
-                    <div class="row justify-content-center mx-1">
-                        <div class="col-md-4">
-                            <div class="form-floating mb-3">
-                                <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
-                                <label for="floatingInput">Last name </label>
+                <div class="card-body">
+                    <form action="" method="post">
+                        <div class="row justify-content-center mx-1">
+                            <div class="col-md-4">
+                                <div class="form-floating mb-3">
+                                    <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
+                                    <label for="floatingInput">Last name </label>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-floating mb-3">
+                                    <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
+                                    <label for="floatingInput">First name </label>
+                                </div>
                             </div>
                         </div>
-                        <div class="col-md-4">
-                            <div class="form-floating mb-3">
-                                <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
-                                <label for="floatingInput">First name </label>
+                        <div class="row justify-content-center mx-1">
+                            <div class="col-md-4">
+                                <div class="form-floating mb-3">
+                                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                                    <label for="floatingInput">Email Address </label>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-floating mb-3">
+                                    <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
+                                    <label for="floatingInput">Username </label>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="row justify-content-center mx-1">
-                        <div class="col-md-4">
-                            <div class="form-floating mb-3">
-                                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                                <label for="floatingInput">Email Address </label>
+                        <div class="row justify-content-center mx-1">
+                            <div class="col-md-4">
+                                <div class="form-floating mb-3">
+                                    <input type="password" class="form-control" id="floatingInput" placeholder="name@example.com">
+                                    <label for="floatingInput">Password </label>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-floating mb-3">
+                                    <input type="password" class="form-control" id="floatingInput" placeholder="name@example.com">
+                                    <label for="floatingInput">Confirm Password </label>
+                                </div>
                             </div>
                         </div>
-                        <div class="col-md-4">
-                            <div class="form-floating mb-3">
-                                <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
-                                <label for="floatingInput">Username </label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row justify-content-center mx-1">
-                        <div class="col-md-4">
-                            <div class="form-floating mb-3">
-                                <input type="password" class="form-control" id="floatingInput" placeholder="name@example.com">
-                                <label for="floatingInput">Password </label>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="form-floating mb-3">
-                                <input type="password" class="form-control" id="floatingInput" placeholder="name@example.com">
-                                <label for="floatingInput">Confirm Password </label>
-                            </div>
-                        </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
 @endsection
