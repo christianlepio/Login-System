25fa9a4eb15d0a5faaded8200e2ea2d7a8bfd353
babaGie <rylepio79@gmail.com>
2022-05-28 22:55:01 +0800

    remove textbox color

--- resources/views/registers.blade.php
+++ resources/views/registers.blade.php
@@ -20,7 +20,7 @@
                             </div>
                             <div class="col-md-4">
                                 <div class="form-floating mb-3">
-                                    <input type="text" class="form-control input is-info" id="floatingInput" placeholder="name@example.com">
+                                    <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
                                     <label for="floatingInput">First name </label>
                                 </div>
                             </div>
