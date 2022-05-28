76ee988ed1e165eb3eb9e73e6e241a987602ca62
babaGie <rylepio79@gmail.com>
2022-05-28 21:47:00 +0800

    update card header font weight

--- resources/views/registers.blade.php
+++ resources/views/registers.blade.php
@@ -3,7 +3,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <div class="container">
             <div class="card mt-5">
-                <div class="card-header fs-4">Registration Module</div>
+                <div class="card-header fs-4 fw-bold">Registration Module</div>
                 <form action="" method="post">
                     <div class="row justify-content-center">
                         <div class="col-md-4">
