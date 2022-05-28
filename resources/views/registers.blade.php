81a2e69b3968ce4e12945d4e5db26e5fa307927c
babaGie <rylepio79@gmail.com>
2022-05-28 22:30:37 +0800

    update card header my

--- resources/views/registers.blade.php
+++ resources/views/registers.blade.php
@@ -3,7 +3,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <div class="container">
             <div class="card mt-5 mx-1">
-                <div class="card-header fs-4 mx-1 fw-bold">
+                <div class="card-header fs-4 my-1 fw-bold">
                     <figure class="mt-1 image is-32x32">
                         <img src="/img/logoko.JPG">
                     </figure>
