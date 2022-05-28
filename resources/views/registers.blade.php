796b5e5bfaed54c64de17ffd81add573d4640175
babaGie <rylepio79@gmail.com>
2022-05-28 22:16:28 +0800

    add bulma css

--- resources/views/registers.blade.php
+++ resources/views/registers.blade.php
@@ -3,7 +3,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <div class="container">
             <div class="card mt-5 mx-1">
-                <div class="card-header fs-4 fw-bold">
+                <div class="card-header is-primary fs-4 fw-bold">
                     <figure class="image is-32x32">
                         <img src="/img/logoko.JPG">
                     </figure>
@@ -14,7 +14,7 @@
                         <div class="row justify-content-center">
                             <div class="col-md-4">
                                 <div class="form-floating mb-3">
-                                    <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
+                                    <input type="text" class="form-control input is-primary" id="floatingInput" placeholder="name@example.com">
                                     <label for="floatingInput">Last name </label>
                                 </div>
                             </div>
