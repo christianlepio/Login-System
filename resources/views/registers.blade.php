a81823fac634cca496fea55e2e09a2077fe86d0a
babaGie <rylepio79@gmail.com>
2022-05-28 22:24:07 +0800

    remove is-primary class in card

--- resources/views/registers.blade.php
+++ resources/views/registers.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <div class="container">
-            <div class="card is-primary mt-5 mx-1">
+            <div class="card mt-5 mx-1">
                 <div class="card-header fs-4 fw-bold">
                     <figure class="image is-32x32">
                         <img src="/img/logoko.JPG">
