89e495c60a0ba541ce27f9ee65715fb457d3706c
babaGie <rylepio79@gmail.com>
2022-05-28 22:24:44 +0800

    update textboxes border color

--- resources/views/registers.blade.php
+++ resources/views/registers.blade.php
@@ -14,13 +14,13 @@
                         <div class="row justify-content-center">
                             <div class="col-md-4">
                                 <div class="form-floating mb-3">
-                                    <input type="text" class="form-control input is-primary" id="floatingInput" placeholder="name@example.com">
+                                    <input type="text" class="form-control input is-info" id="floatingInput" placeholder="name@example.com">
                                     <label for="floatingInput">Last name </label>
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="form-floating mb-3">
-                                    <input type="text" class="form-control input is-primary" id="floatingInput" placeholder="name@example.com">
+                                    <input type="text" class="form-control input is-info" id="floatingInput" placeholder="name@example.com">
                                     <label for="floatingInput">First name </label>
                                 </div>
                             </div>
@@ -28,13 +28,13 @@
                         <div class="row justify-content-center">
                             <div class="col-md-4">
                                 <div class="form-floating mb-3">
-                                    <input type="email" class="form-control input is-primary" id="floatingInput" placeholder="name@example.com">
+                                    <input type="email" class="form-control input is-info" id="floatingInput" placeholder="name@example.com">
                                     <label for="floatingInput">Email Address </label>
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="form-floating mb-3">
-                                    <input type="text" class="form-control input is-primary" id="floatingInput" placeholder="name@example.com">
+                                    <input type="text" class="form-control input is-info" id="floatingInput" placeholder="name@example.com">
                                     <label for="floatingInput">Username </label>
                                 </div>
                             </div>
@@ -42,13 +42,13 @@
                         <div class="row justify-content-center">
                             <div class="col-md-4">
                                 <div class="form-floating mb-3">
-                                    <input type="password" class="form-control input is-primary" id="floatingInput" placeholder="name@example.com">
+                                    <input type="password" class="form-control input is-info" id="floatingInput" placeholder="name@example.com">
                                     <label for="floatingInput">Password </label>
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="form-floating mb-3">
-                                    <input type="password" class="form-control input is-primary" id="floatingInput" placeholder="name@example.com">
+                                    <input type="password" class="form-control input is-info" id="floatingInput" placeholder="name@example.com">
                                     <label for="floatingInput">Confirm Password </label>
                                 </div>
                             </div>
